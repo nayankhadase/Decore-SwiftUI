1e12fcb15db5e726dc108ed88fa105bfef3dabe4
nayankhadase <khadsenayan1996@gmail.com>
2022-02-04 12:24:47 +0530

    Initial Commit

---
+++ Decore-SwiftUI/Decore_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Decore_SwiftUIApp.swift
+//  Decore-SwiftUI
+//
+//  Created by Nayan Khadase on 04/02/22.
+//
+
+import SwiftUI
+
+@main
+struct Decore_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
